ae03594650c40afdae0f906f2e78a3bba8ae770b
Ahmed Amine Hassou <japnoo@gmail.com>
2018-04-08 22:25:16 +0100

    Attention: letters are now rotating!

--- DMOJ/ccc13j2.py
+++ DMOJ/ccc13j2.py
@@ -1,6 +1,6 @@
 word = input()
 trueLetters = ("I", "O", "S", "H", "Z", "X", "N")
-if any(letter not in word for letter in trueLetters):
+if any(letter not in trueLetters for letter in word):
     print("NO")
 else:
     print("YES")
