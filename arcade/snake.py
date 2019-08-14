a1fab6501d5405ca0a969d3ed7ba0b574b3448eb
Ahmed Amine Hassou <japnoo@gmail.com>
2019-08-14 04:32:26 +0100

    Got synchronized movement to work kind of.

--- arcade/snake.py
+++ arcade/snake.py
@@ -3,29 +3,17 @@ import random
 import math
 SCREEN_WIDTH = 640
 SCREEN_HEIGHT = 480
-MOVEMENT_SPEED = 1
+INDENT = 2
+SNAKE_SCALE = 25
 
-class Snake:
-    def __init__(self, start_x, start_y, end_x, end_y, change_x, change_y):    
-        self.start_x = start_x
-        self.start_y = start_y
-        self.end_x = end_x
-        self.end_y = end_y
-        self.change_x = change_x
-        self.change_y = change_y
+class Snake(arcade.Sprite):
+    def spawn(self):
+        self.center_x = random.randint(SNAKE_SCALE, SCREEN_WIDTH - SNAKE_SCALE)
+        self.center_y = random.randint(SNAKE_SCALE, SCREEN_HEIGHT - SNAKE_SCALE)
+       
 
-    def draw(self):
-        # Draw the snake with the instance variables.
-        arcade.draw_line(self.start_x, self.start_y, self.end_x, self.end_y, arcade.color.RED, 2)
-    
-    def update(self):
-        # Move the snake
-        self.start_x += self.change_x
-        self.start_y += self.change_y
-        self.end_x += self.change_x
-        self.end_y += self.change_y        
 '''
-class dot(arcade.sprite):
+cla ss dot(arcade.sprite):
     self.center_x = random.randint(RADIUS, SCREEN_WIDTH - RADIUS)
     self.center_y = random.randint(RADIUS, SCREEN_HEIGHT - RADIUS)
 '''
@@ -34,30 +22,61 @@ class MyGame(arcade.Window):
         # Call the parent class's init function
         super().__init__(width, height, title)
         self.set_mouse_visible(False)
+        self.player_list = None
+        self.dot_list = None
+        self.player_sprite = None
+        self.X_SPEED = 0
+        self.Y_SPEED = 0
+        self.counter = 0
         arcade.set_background_color(arcade.color.BABY_BLUE)
-        self.snake = Snake(2, 2, 50, 2, 0, 0)
 
     def on_draw(self):
         # Called whenever we need to draw the window.
         arcade.start_render()
-        self.snake.draw()
-    def update(self, delta_time):
-        self.snake.update()
+        self.player_list.draw()
+
+    def setup(self):
+        self.snake = Snake("dot.png", SNAKE_SCALE)
+        self.snake.spawn()
+        self.player_list = arcade.SpriteList()
+        self.player_list.append(self.snake)
+        for x in range(1, 4):
+            dot = arcade.Sprite('dot.png', SNAKE_SCALE)
+            dot.center_x = self.player_list[0].center_x - (SNAKE_SCALE*x)
+            dot.center_y = self.player_list[0].center_y
+            self.player_list.append(dot)
 
     def on_key_press(self, key, modifiers):
         if key == arcade.key.LEFT:
-            self.snake.change_x = -MOVEMENT_SPEED
+            self.X_SPEED = -2
+            self.Y_SPEED = 0
         elif key == arcade.key.RIGHT:
-            self.snake.change_x = MOVEMENT_SPEED
+            self.X_SPEED = 2
+            self.Y_SPEED = 0
         elif key == arcade.key.UP:
-            self.snake.change_y = MOVEMENT_SPEED
+            self.X_SPEED = 0
+            self.Y_SPEED = 2
         elif key == arcade.key.DOWN:
-            self.snake.change_y = -MOVEMENT_SPEED
+            self.X_SPEED = 0
+            self.Y_SPEED = -2
+          
+    
+    def update(self, delta_time):
+        for index in range(1, len(self.player_list)):
+            LAST_MOVED_X = self.player_list[index-1].center_x
+            LAST_MOVED_Y = self.player_list[index-1].center_y
+            self.player_list[index].center_x = LAST_MOVED_X
+            self.player_list[index].center_y = LAST_MOVED_Y
 
-
+        self.player_list[0].center_x += self.X_SPEED
+        self.player_list[0].center_y += self.Y_SPEED
+        
+        print(self.X_SPEED, self.Y_SPEED)
+       
 
 def main():
     window = MyGame(640, 480, "Drawing Example")
+    window.setup()
     arcade.run()
 
 
